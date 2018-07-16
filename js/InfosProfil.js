488a783a70f0da4eb342b0e3022e193f3a219f36
Osman <31457176+wadgreen@users.noreply.github.com>
2018-07-16 14:26:26 +0200

    Update InfosProfil.js

--- js/InfosProfil.js
+++ js/InfosProfil.js
@@ -1,37 +1,31 @@
-export class InfosProfil{
-    // constructor(profil, laura, message, prenom, nom, telephone, idClient, image, ...dent) {
-    constructor(profil) {
-        // {profil:"fff", message:"sddsd",}
-        this.profil = profil;
-        this.laura = profil.laura;
-        this.message = profil.message;
-        this.nom = profil.nom;
-        this.prenom = profil.prenom;
-        this.telephone = profil.telephone;
-        this.idClient =  profil.idClient;
-        this.image = profil.image;
-        this.dents = profil.dent;
+export class InfosProfil {
+    constructor(codePatient, prenom, nom, telephone, photoProfil, photoFaceAv, photoFaceAp, photoIntraAv, photoIntraAp, photoRadioAv, photoRadioAp) {
+        this.codePatient = codePatient;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.telephone = telephone;
+        // this.dateNaissance = profil.dateNaissance;
+        this.photoProfil = photoProfil;
+        this.photoFaceAv = photoFaceAv;
+        this.photoFaceAp = photoFaceAp;
+        this.photoIntraAv = photoIntraAv;
+        this.photoIntraAp = photoIntraAp;
+        this.photoRadioAv = photoRadioAv;
+        this.photoRadioAp = photoRadioAp;
         this.recupererInfosProfil();
     }
     recupererInfosProfil() {
-        document.querySelector(".center").textContent =  this.profil;
-        document.querySelector("#lau").textContent =  this.laura;
-        document.querySelector("#message").textContent =  this.message;
-        document.querySelector(".prenom").value = this.prenom;
-        document.querySelector(".nom").innerText = this.nom;
-        document.querySelector(".tel").value = this.telephone;
-        document.querySelector(".idclient").value = this.idClient;
-        document.querySelector("#circle").src = this.image;
-        
-        this.dentsEl = document.querySelector("#dents div div img");
-        
-        for(let d in this.dentsEl){
-            console.log(d);
-            this.dentsEl[d].src = this.dents[d];
-        }
-        // document.querySelector("#d1").src = this.dent1;
-        // document.querySelector("#d2").src = this.dent2;
-        // document.querySelector("#d3").src = this.dent3;
-        // document.querySelector("#d4").src = this.dent4;
+        document.getElementById("key").value = this.codePatient;
+        document.getElementById("prenom").value = this.prenom;
+        document.getElementById("nom").value = this.nom;
+        document.getElementById("phone").value = this.telephone;
+        // document.getElementById("#").date = this.dateNaissance;
+        document.getElementById("profil").src = this.photoProfil;
+        document.getElementById("faceAv").src = this.photoFaceAv;
+        document.getElementById("faceAp").src = this.photoFaceAp;
+        document.getElementById("IntraAv").src = this.photoIntraAv;
+        document.getElementById("IntraAp").src = this.photoIntraAp;
+        // document.getElementById("d4").src = this.photoRadioAv;
+        // document.getElementById("d4").src = this.photoRadioAp;
     }
 }
