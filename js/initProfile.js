1f98f7e32d1540ca66dbc978fb57d63051ad40f4
Osman <31457176+wadgreen@users.noreply.github.com>
2018-07-26 13:30:58 +0200

    Update initProfile.js

--- js/initProfile.js
+++ js/initProfile.js
@@ -1,15 +1,16 @@
-import { InfosProfil } from './InfosProfil.js';
+import {
+    InfosProfil
+} from './InfosProfil.js';
 
 class InitProfile {
     constructor() {
         this.request = new XMLHttpRequest();
-        this.request.open('GET','data/profil.json');
+        this.request.open('GET', 'data/profil.json');
         this.request.responseType = 'json';
         this.request.send();
         this.request.onload = () => {
             this.jsonObject = this.request.response;
-            this.Profil = new InfosProfil (this.jsonObject.codePatient,this.jsonObject.prenom,this.jsonObject.nom,this.jsonObject.telephone,this.jsonObject.photoProfil,this.jsonObject.photoFaceAv,this.jsonObject.photoFaceAp,this.jsonObject.photoIntraAv, this.jsonObject.photoIntraAp, this.jsonObject.photoRadioAv, this.jsonObject.photoRadioAp);
-            //  console.log(this.jsonObject.photoRadioAv);
+            this.Profil = new InfosProfil(this.jsonObject);
         }
     }
 }
